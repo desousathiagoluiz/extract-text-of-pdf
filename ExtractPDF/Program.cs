1575682c74eb9762a2e9d2402686df9083543085
ThiaGão <desousathiagoluiz@gmail.com>
2024-09-26 16:50:18 -0300

    extract-to-PDF

---
+++ ExtractPDF/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+
+namespace Ler_PDF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pdfPath = @"C:\New-C#-Projects\extract-text-of-pdf\cert-simple.pdf";
+            string text = ExtractTextFromPdf(pdfPath);
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+
+        static string ExtractTextFromPdf(string pdfPath)
+        {
+            StringBuilder text = new StringBuilder();
+            using (PdfReader pdfReader = new PdfReader(pdfPath))
+            using (PdfDocument pdfDoc = new PdfDocument(pdfReader))
+            {
+                for (int i = 1; i <= pdfDoc.GetNumberOfPages(); i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(i)));
+                }
+            }
+            return text.ToString();
+        }
+    }
+}
